13eb8f7efa95c2a704a032259bca53ad4782988a
Dahamboseth <bosethwanapura@gmail.com>
2025-01-30 18:42:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=Q8o32T6Y#MiZAjyqkHKgHaZYKKOi6iCwmBweIW1uVeuabt4VI3ow",
+SESSION_ID: process.env.SESSION_ID || "KAVI-EXE=33hkHQ6a#Fx_GPByqUViuE5eOekE1xc6_6nEvN1ewzDoYwLTN92E",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
+ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "true",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
 ANTI_CALL: process.env.ANTI_CALL || "false",
